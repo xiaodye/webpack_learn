02635fe67b5dffb557746ac3d7e8697700c76494
lansen <bluewoods.lan@gmail.com>
2023-06-09 21:12:04 +0800

    feat: core-js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import count from "./js/count";
 import sum from "./js/sum";
 import { mul } from "./js/math";
+
+// import "core-js"; // 完整引入
+import "core-js/es/promise"; // 按需引入
+
 import "./css/style.css";
 import "./css/common.less";
 import "./css/common.scss";
@@ -9,3 +13,7 @@ import "./css/iconfont.css";
 console.log(count(3, 2));
 console.log(sum(3, 2));
 console.log(mul(2, 3));
+
+const arr = [1, 23, 4];
+
+console.log(arr.includes(4));
--- src/js/count.js
+++ src/js/count.js
@@ -1,3 +1,9 @@
 export default function count(a, b) {
   return a - b;
 }
+
+new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("fulfilled");
+  }, 1000);
+}).then((res) => console.log(res));
